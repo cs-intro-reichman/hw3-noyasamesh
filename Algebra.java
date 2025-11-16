8a30fca62e1dfd9c2805152bb7369bd3c917065a
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-16 14:50:39 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,90 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = x1;
+		for(int i=0; i<x2; i++){
+			sum++;
+		}
+		
+		return sum;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+        int sum = x1;
+		for(int i=0; i<x2; i++){
+			sum--;
+		}	
+		return sum;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = 0;
+		if (x2==1){
+			return x1;
+		}
+		if (x1==0 || x2==0){
+			return 0;
+		}
+		for(int i=0; i<x2; i++){
+			sum = plus(x1, sum);
+		}
+		return sum;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int temp=1;
+		for(int i=0;i<n;i++){
+			temp= times(x, temp);
+			
+		}
+		return temp;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int count=0;
+		if(x1==x2)
+		return 1;
+		if(x2==1)
+		return x1;
+		while(x1!=x2){
+			if(x1<x2)
+			return count;
+			else {
+				x1= minus(x1, x2);
+			    count++;
+			}
+		}
+		count++;
+		return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
+		while (x1>=x2) {
+			x1= minus(x1, x2);
+		}
+		if (x1==0)
 		return 0;
+		else 
+		return x1;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int i=1;
+		int y=div(x,i);
+			while (y >= i) {
+				if(y == i)
+				return i;
+				i++;
+				y = div(x,i);
+				
+			}
+		i--;
+		return i;
 	}	  	  
 }
--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,66 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String s1 = preProcess(str1);
+		String s2 = preProcess(str2);
+
+		for(int i=0; i<s1.length(); i++) {
+			if (s1.charAt(i)==32)
+			s1= s1.substring(0, i)+ s1.substring(i+1);
+		}
+
+		for(int i=0;i <s2.length(); i++) {
+			if (s2.charAt(i)==32)
+			s2= s2.substring(0, i)+ s2.substring(i+1);
+		}
+
+		if (s1.length() != s2.length()){
+			return false;
+		}
+
+		for(int i=0; i<s1.length();i++) {
+			char c = s1.charAt(i);
+			int j = s2.indexOf(c);
+			if (j == -1){
+				return false;
+			}
+
+			s2 = s2.substring(0, j) + s2.substring(j+1);
+
+			}
+		
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		String newStr="";
+		for (int i=0; i< str.length();i++) {
+			char c = str.charAt(i);
+			if (c==32){
+				newStr+= c;
+			}
+			if ((c>96) && (c<123)) {
+				newStr += c;
+			}
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		int x;
+		String newStr = "";
+		while (str.length() >0) {
+			x= (int) (Math.random() * str.length());
+			newStr += str.charAt(x);
+			str = str.substring(0, x) + str.substring(x+1);
+
+		}
+		return newStr;
 	}
 }
